131d7d66f13ce132534866e9448b47a518bf6a1d
Arunshankar <arunshankar.n@outlook.com>
2019-03-23 12:52:30 -0700

    added test for async API

--- src/components/Description.test.js
+++ src/components/Description.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import waitUntil from 'async-wait-until';
+import { mount, shallow, configure } from 'enzyme';
 import Description from './Description';
 
-it('Description renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Description ind='1' id='121003' key='121003'/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+it('Description fetching item from API', async (done) => {
+     const root = mount(<Description ind='1' id='121003' key='121003' />);
+     await waitUntil(() => root.state('info').hasOwnProperty('id'));
+     expect(root.state('info').id).toEqual(121003);
+     done();
+ });
--- src/components/NewsFeeds.test.js
+++ src/components/NewsFeeds.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import waitUntil from 'async-wait-until';
+import { mount, shallow, configure } from 'enzyme';
 import NewsFeeds from './NewsFeeds';
 
-it('NewsFeeds renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NewsFeeds/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+it('NewsFeeds fetching ids from API', async (done) => {
+     const root = mount(<NewsFeeds />);
+     await waitUntil(() => root.state('ids').length);
+     expect(root.state('ids')[0]).toBeGreaterThan(-1);
+     done();
+ });
